6042f14771e4583457c2217a47845b98c1b5b086
ASintsova <ansintsova@biol.ethz.ch>
2022-06-28 18:41:51 +0200

    minimal version finished

--- app.py
+++ app.py
@@ -1,38 +1,42 @@
 import streamlit as st
 import tarfile
 from pathlib import Path
+import re
 import yaml
+import pandas as pd
 
 # Custom imports
 from multipage import MultiPage
-from pages import EDA, DiffAb, Summary, Pathway, Assembly, Expression, Home
+from options import EDA, DiffAb, Summary, Pathway, Assembly, Expression, Home
 st.set_page_config(page_title="NCCR Microbiomes ETHZ", layout='wide')
 
 #DATADIR = Path('/Users/ansintsova/git_repos/tnseq_app/data/ath_rnaseq')
 #DATADIR = Path('/Users/ansintsova/git_repos/tnseq_app/data/results_for_app')
 
-DATADIR = Path(".")
+st.title(":microscope: Welcome to MiBio App :microscope:")
 fname = st.sidebar.file_uploader('Project Results', accept_multiple_files=False)
+
 if not fname:
     st.stop()
 tar = tarfile.open(fileobj=fname, mode="r:gz")
 tar.extractall()
+DATADIR = Path(tar.getnames()[0])
+config_file = DATADIR/'pages.yaml'
 
-
-
-with open("pages.yaml") as fh:
+with open(config_file) as fh:
     config = yaml.safe_load(fh)
-st.stop()
-st.title("NCCR Microbiomes ETHZ")
+
+
 
 # Create an instance of the app
 app = MultiPage()
 project_sites = config['pages']
 
-# results = {
-#            'Summary': ('Summary', Summary.app, DATADIR),
-#            'Path': ('Pathway Analysis',  Pathway.app, DATADIR),
-#            'Assembly': ('Assembly', Assembly.app, DATADIR)}
+# # results = {
+# #            'Summary': ('Summary', Summary.app, DATADIR),
+# #            'Path': ('Pathway Analysis',  Pathway.app, DATADIR),
+# #            'Assembly': ('Assembly', Assembly.app, DATADIR)}
+#
 
 available_pages = {'Home': ('Home', Home.app, DATADIR),
            'EDA': ('Exploratory Data Analysis', EDA.app, DATADIR),
--- pages/Summary.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-
-def app():
-    st.write('## Summary')
-    st.write('Below you can see the quality controls and summary of the data analysis')
